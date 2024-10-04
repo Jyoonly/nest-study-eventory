3c63d4706a246051d234bb51189488fd72f09315
jyLee <1001baam@gmail.com>
2024-10-04 21:45:05 +0900

    Week1-HW category 추가

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configModule } from './modules/config.module';
 import { LoggerMiddleware } from '../common/middlewares/logger.middleware';
+import { CategoryModule } from 'src/category/category.module';
 import { RegionModule } from '../region/region.module';
 import { CommonModule } from '../common/common.module';
 
 @Module({
-  imports: [configModule, RegionModule, CommonModule],
+  imports: [configModule, CategoryModule, RegionModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
