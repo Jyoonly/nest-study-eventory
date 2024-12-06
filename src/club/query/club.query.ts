0de0fe2b0c59d438a7647609c3243e9d13eb83b2
jyLee <1001baam@gmail.com>
2024-12-06 22:51:37 +0900

    feat: #102 클럽 여러개 조회 기능 추가
    - hostId 또는 name으로 검색 가능.

---
+++ src/club/query/club.query.ts
@@ -0,0 +1,21 @@
+import { ApiPropertyOptional } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsIn, IsInt, IsOptional } from "class-validator";
+
+export class ClubQuery {
+    @IsOptional()
+    @IsInt()
+    @Type(() => Number)
+    @ApiPropertyOptional({
+        description: '호스트 ID',
+        type: Number,
+    })
+    hostId?: number;
+
+    @IsOptional()
+    @ApiPropertyOptional({
+        description: '클럽 이름',
+        type: String,
+    })
+    name?: string;
+}
