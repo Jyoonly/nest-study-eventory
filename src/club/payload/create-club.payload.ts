5da50d2e653566d35ef00f84a2601a1de3b195cf
jyLee <1001baam@gmail.com>
2024-12-06 19:14:50 +0900

    fix: 오타 수정

--- src/club/payload/create-club.payload.ts
+++ src/club/payload/create-club.payload.ts
@@ -1,16 +1,15 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { Type } from 'class-transformer';
 import {
-  IsArray,
-  IsDate,
-  IsInt,
-  IsPositive,
-  IsString,
-  Min,
+    IsArray,
+    IsDate,
+    IsInt,
+    IsPositive,
+    IsString,
+    Min,
 } from 'class-validator';
 
 export class CreateClubPayload {
-<<<<<<< HEAD
     /*
     @IsInt()
     @IsPositive()
@@ -26,35 +25,19 @@ export class CreateClubPayload {
         type: String,
     })
     name!: string;
-=======
-  @IsInt()
-  @IsPositive()
-  @ApiProperty({
-    description: '호스트 ID',
-    type: Number,
-  })
-  hostId!: number;
 
-  @IsString()
-  @ApiProperty({
-    description: '이름',
-    type: String,
-  })
-  name!: string;
->>>>>>> 88836ec2e0e8152b2709dc5b4d7fd1879cba7f01
+    @IsString()
+    @ApiProperty({
+        description: '설명',
+        type: String,
+    })
+    description!: string;
 
-  @IsString()
-  @ApiProperty({
-    description: '설명',
-    type: String,
-  })
-  description!: string;
-
-  @Min(2)
-  @IsInt()
-  @ApiProperty({
-    description: '최대 인원',
-    type: Number,
-  })
-  maxPeople!: number;
+    @Min(2)
+    @IsInt()
+    @ApiProperty({
+        description: '최대 인원',
+        type: Number,
+    })
+    maxPeople!: number;
 }
